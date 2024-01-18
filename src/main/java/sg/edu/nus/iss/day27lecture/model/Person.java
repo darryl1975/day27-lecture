67f9db4d1fce888059ea95283dae5ab045536ac8
darryl1975 <darryl1975@hotmail.com>
2024-01-18 09:54:31 +0800

    added Person model

---
+++ src/main/java/sg/edu/nus/iss/day27lecture/model/Person.java
@@ -0,0 +1,69 @@
+package sg.edu.nus.iss.day27lecture.model;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "persons")
+public class Person {
+    
+    @Id
+    private String personId;
+    private String name;
+    private long age;
+    private String gender;
+    private List<String> hobbies;
+
+    public Person(String personId, String name, long age, String gender, List<String> hobbies) {
+        this.personId = personId;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.hobbies = hobbies;
+    }
+
+    public String getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(String personId) {
+        this.personId = personId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getAge() {
+        return age;
+    }
+
+    public void setAge(long age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public List<String> getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(List<String> hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    
+    
+    
+}
